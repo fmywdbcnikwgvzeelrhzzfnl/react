4c9fa9e05466f4aa927330fd7c20400984ed0d63
fmywdbcnikwgvzeelrhzzfnl <mzyvjmxjjdwvg@mail.ru>
2018-06-22 00:59:32 +0300

    домашняя страница без состояний и событий готова

---
+++ src/components/PagesPanel/PagesPanel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './PageTitle.css';
+
+export default class PageTitle extends React.Component {
+    render() {
+        return (
+            <div className="postTitle">
+                <h1>Page title</h1>
+            </div>
+        );
+    }
+}
